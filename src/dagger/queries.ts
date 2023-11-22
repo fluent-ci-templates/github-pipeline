ff0e77749f724216a478797e4aafc68d3938b444
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-22 11:51:22 +0000

    update queries

--- src/dagger/queries.ts
+++ src/dagger/queries.ts
@@ -1,7 +1,12 @@
 import { gql } from "../../deps.ts";
 
 export const releaseUpload = gql`
-  query ReleaseUpload($src: String, $tag: String!, $file: String!) {
-    releaseUpload(src: $src, tag: $tag, file: $file)
+  query ReleaseUpload(
+    $src: String
+    $tag: String!
+    $file: String!
+    $token: String!
+  ) {
+    releaseUpload(src: $src, tag: $tag, file: $file, token: $token)
   }
 `;
