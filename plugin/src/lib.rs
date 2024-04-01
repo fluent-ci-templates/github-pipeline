3e2c67d630977cf5ee8d69b0dde62ca4dfb6c3ad
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-04-01 21:20:54 +0000

    feat: add wasm plugin
    feat: add wasm plugin

---
+++ plugin/src/lib.rs
@@ -0,0 +1,13 @@
+use extism_pdk::*;
+use fluentci_pdk::dag;
+
+#[plugin_fn]
+pub fn release_upload(args: String) -> FnResult<String> {
+    let stdout = dag()
+        .pipeline("release_upload")?
+        .pkgx()?
+        .with_exec(vec!["pkgx", "install", "gh", "git"])?
+        .with_exec(vec!["gh", "release", "upload", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
